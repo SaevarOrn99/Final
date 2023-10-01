b3bf9777275cecce93ab95f3d806b6cfd1ea1993
SaevarOrn99 <Saevarorn99@gmail.com>
2023-10-01 15:04:36 +0000

    x

--- Evil_bit.cpp
+++ Evil_bit.cpp
@@ -4,9 +4,9 @@
 
 unsigned short csum(unsigned short *ptr,int nbytes) //calculates the checksum
 {
-	register long sum; 
-	unsigned short oddbyte;
-	register short answer;
+	register long sum; // 32 bit register a long sum
+	unsigned short oddbyte; // 16 bit
+	register short answer; // 16 bit
 
 	sum=0;
 	while(nbytes>1) { // This loops through the packet and sums up the bytes
@@ -23,7 +23,7 @@ unsigned short csum(unsigned short *ptr,int nbytes) //calculates the checksum
 	sum = sum + (sum>>16);
 	answer=(short)~sum;
 	
-	return(answer);
+	return(answer); // returns the checksum
 }	
 
 
@@ -55,18 +55,18 @@ int getUDPpackageRaw(const char* ip, int port, u_int32_t XOR) {
     // Insert the getsockname call here
     struct sockaddr_in localAddress;
     socklen_t addressLength = sizeof(localAddress);
-    if (getsockname(udpsock, (struct sockaddr*)&localAddress, &addressLength) == -1) {
+    if (getsockname(udpsock, (struct sockaddr*)&localAddress, &addressLength) == -1) { // Get the local address
         perror("getsockname");
         close(udpsock);
         return -1;
     }
-    int assignedPort = ntohs(localAddress.sin_port);
+    int assignedPort = ntohs(localAddress.sin_port); // Get the port number
 	close(udpsock); // Close the socket
 
 
 
     //Create a raw socket of type IPPROTO
-	int raw_socket = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+	int raw_socket = socket(AF_INET, SOCK_RAW, IPPROTO_RAW); // Raw socket
 	if(raw_socket == -1)
 	{
 		//socket creation failed, may be because of non-root privileges
@@ -74,17 +74,17 @@ int getUDPpackageRaw(const char* ip, int port, u_int32_t XOR) {
 		exit(1);
 	}
 	
-	char datagram[4096] , source_ip[32] , *data , *pseudogram;
+	char datagram[4096] , source_ip[32] , *data , *pseudogram; 
 	memset(datagram, 0, 4096);//zero out the packet buffer
 
 	//IP header
-	struct iphdr *iph = (struct iphdr *) datagram;
+	struct iphdr *iph = (struct iphdr *) datagram; // IP header
 	
 	//UDP header
-	struct udphdr *udph = (struct udphdr *) (datagram + sizeof (struct ip));
+	struct udphdr *udph = (struct udphdr *) (datagram + sizeof (struct ip)); // UDP header
 	
-	struct sockaddr_in sin; // fyrri destination
-	struct pseudo_header1 psh;
+	struct sockaddr_in sin; 
+	struct pseudo_header1 psh; 
 	
 	//Data part
 	data = datagram + sizeof(struct iphdr) + sizeof(struct udphdr);
@@ -93,28 +93,28 @@ int getUDPpackageRaw(const char* ip, int port, u_int32_t XOR) {
 	
 	
 	//Create dummy socket
-	int Dummy_sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	int Dummy_sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP); // cretes a dummy socket for listening
     if (Dummy_sock < 0) {
         perror("Error creating UDP socket");
         return -1;
     }
 
 	int yes = 1;
-	if (setsockopt(Dummy_sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) < 0) {
+	if (setsockopt(Dummy_sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) < 0) { // set the socket options
     	perror("Error setting socket options");
     	close(Dummy_sock);
     	return -1;
 	}
 
-    struct sockaddr_in localAddr;
-    memset(&localAddr, 0, sizeof(localAddr));
-    localAddr.sin_family = AF_INET;
-    localAddr.sin_port = htons(assignedPort); // setja random port hérna
-   	localAddr.sin_addr.s_addr = INADDR_ANY;
+    struct sockaddr_in localAddr; // local address
+    memset(&localAddr, 0, sizeof(localAddr)); // Zero out memory of address struct
+    localAddr.sin_family = AF_INET; // IPv4
+    localAddr.sin_port = htons(assignedPort); // assigneing the port from earlier to receive the messege the raw socket sent.
+   	localAddr.sin_addr.s_addr = INADDR_ANY; // any address
 
 
 	// Binda dummy socketið
-    if (bind(Dummy_sock, (struct sockaddr*) &localAddr, sizeof(localAddr)) < 0) {
+    if (bind(Dummy_sock, (struct sockaddr*) &localAddr, sizeof(localAddr)) < 0) { // bind the socket to the local address on the computer
         perror("Error binding UDP socket");
         close(Dummy_sock);
         return -1;
@@ -122,69 +122,69 @@ int getUDPpackageRaw(const char* ip, int port, u_int32_t XOR) {
 
 
 
-	//some address resolution
-	strcpy(source_ip , ip); // source ip getur verið villa hérna !!!!!!!
+	//address resolution
+	strcpy(source_ip , ip); 
 
-	// connect a þetta dæmi 
-	memset(&sin, 0, sizeof(sin));
-	sin.sin_family = AF_INET;
-	sin.sin_port = htons(port); // þetta er portið sem ég sendi á
+	// The destination server 
+	memset(&sin, 0, sizeof(sin)); // Zero out memory of address struct
+	sin.sin_family = AF_INET; // IPv4
+	sin.sin_port = htons(port); // assigns the port to the sin struct
 
-	sin.sin_addr.s_addr = inet_addr (ip); // skoða skoða raw socketið
+	sin.sin_addr.s_addr = inet_addr (ip); // assigns the ip to the sin struct
 	
 	//Fill in the IP Header
-	iph->ihl = 5;
-	iph->version = 4;
-	iph->tos = 0;
-	iph->tot_len = sizeof (struct iphdr) + sizeof (struct udphdr) + strlen(data);
+	iph->ihl = 5; // header length
+	iph->version = 4; // IPv4
+	iph->tos = 0; // Type of service
+	iph->tot_len = sizeof (struct iphdr) + sizeof (struct udphdr) + strlen(data); // total length of the packet
 	iph->id = htonl(54321);	//Id of this packet
 	iph->frag_off = htons(0x8000); // Setting the evil bit in the fragment offset field
 
-	iph->ttl = 255;
-	iph->protocol = IPPROTO_UDP;
+	iph->ttl = 255; // time to live
+	iph->protocol = IPPROTO_UDP; // UDP
 	iph->check = 0;		//Set to 0 before calculating checksum
-	iph->saddr =  localAddr.sin_addr.s_addr; // þetta þarf að vera annað ipið sem ég sendi á
-	iph->daddr =  inet_addr (ip);	//Spoof the source ip address
+	iph->saddr =  localAddr.sin_addr.s_addr; // Source address
+	iph->daddr =  inet_addr (ip);	//Destination address
 	
 	//Ip checksum
-	iph->check = csum ((unsigned short *) datagram, iph->tot_len);
+	iph->check = csum ((unsigned short *) datagram, iph->tot_len); // The checksum
 	
 	//UDP header
-	udph->source = htons(assignedPort);
-	udph->dest = sin.sin_port; // þarf að setja range hérna Skoða !!!!! for lykkja? eða 
+	udph->source = htons(assignedPort); // assigns the port from earlier to the source port
+	udph->dest = sin.sin_port; // assigns the port from earlier to the destination port
 	udph->len = htons(8 + strlen(data));	//tcp header size
 	udph->check = 0;	//leave checksum 0 now, filled later by pseudo header
 	
 	//Now the UDP checksum using the pseudo header
-	psh.source_address = inet_addr(source_ip);
-	psh.dest_address = sin.sin_addr.s_addr;
-	psh.placeholder = 0;
-	psh.protocol = IPPROTO_UDP;
-	psh.udp_length = htons(sizeof(struct udphdr) + strlen(data) );
+	psh.source_address = inet_addr(source_ip); // Assign the phsudo header to the source ip
+	psh.dest_address = sin.sin_addr.s_addr;  // Assign the phsudo header to the destination ip
+	psh.placeholder = 0; 
+	psh.protocol = IPPROTO_UDP; // UDP
+	psh.udp_length = htons(sizeof(struct udphdr) + strlen(data) ); // UDP header size
 	
-	int psize = sizeof(struct pseudo_header1) + sizeof(struct udphdr) + strlen(data);
-	pseudogram = (char*) malloc(psize);
+	int psize = sizeof(struct pseudo_header1) + sizeof(struct udphdr) + strlen(data); // size of the phsudo header
+	pseudogram = (char*) malloc(psize); // allocate memory for the phsudo header
 
 	
-	memcpy(pseudogram , (char*) &psh , sizeof (struct pseudo_header1));
-	memcpy(pseudogram + sizeof(struct pseudo_header1) , udph , sizeof(struct udphdr) + strlen(data));
+	memcpy(pseudogram , (char*) &psh , sizeof (struct pseudo_header1)); // copy the phsudo header to the pseudogram
+	memcpy(pseudogram + sizeof(struct pseudo_header1) , udph , sizeof(struct udphdr) + strlen(data)); // copy the UDP header to the pseudogram
 	
-	udph->check = csum( (unsigned short*) pseudogram , psize);
+	udph->check = csum( (unsigned short*) pseudogram , psize); // calculate the checksum for the UDP header
 
 
 	// connecta áður en ég sendi pakkann og bind líka útaf annars fæ ég aldrei pakkana
 	// Connect to the server
-    if (connect(raw_socket, (struct sockaddr*)&sin, sizeof(sin)) < 0) {
-        std::cerr << "Error: Could not connect to server." << std::endl;
+    if (connect(raw_socket, (struct sockaddr*)&sin, sizeof(sin)) < 0) { // connect to the server
+        std::cerr << "Error: Could not connect to server." << std::endl; 
         close(raw_socket);
         return 1;
     }
 
-    std::cout << "Connected successfully to the server." << std::endl;
+    std::cout << "Connected successfully to the server." << std::endl; 
 
 	// Continue looping until the expected message is received
 	char portStr[5]; // For extracting 4 characters plus null terminator
-	while (true) {
+	while (true) { // Loop until the expected message is received
 		// Send the packet
 			if (sendto (raw_socket, datagram, iph->tot_len ,	0, (struct sockaddr *) &sin, sizeof (sin)) < 0)
 		{
@@ -196,7 +196,7 @@ int getUDPpackageRaw(const char* ip, int port, u_int32_t XOR) {
 		// Receive a packet
 		char buffer[1024];
 		socklen_t addrSize = sizeof(localAddr);
-		int bytesReceived = recvfrom(Dummy_sock, buffer, sizeof(buffer), 0, (struct sockaddr*)&localAddr, &addrSize);
+		int bytesReceived = recvfrom(Dummy_sock, buffer, sizeof(buffer), 0, (struct sockaddr*)&localAddr, &addrSize); // Receive a message
 		if (bytesReceived < 0) {
 			perror("Error receiving data");
 			return -1;
@@ -206,7 +206,7 @@ int getUDPpackageRaw(const char* ip, int port, u_int32_t XOR) {
 		std::cout << "Received message from port "<< port << " is: " << buffer << std::endl;
 
 		// Check for the expected message using strstr()
-		if (strstr(buffer, "Yes, strong in the dark side you are")) {
+		if (strstr(buffer, "Yes, strong in the dark side you are")) { // If the message is the expected message
 			strncpy(portStr, &buffer[bytesReceived - 4], 4);
 			portStr[4] = '\0';  // Null terminate the string
 			break; // Exit the loop
@@ -215,5 +215,5 @@ int getUDPpackageRaw(const char* ip, int port, u_int32_t XOR) {
 
 	close(Dummy_sock); // Close the listening socket
 	close(raw_socket); // Close the socket before returning
-	return atoi(portStr);
+	return atoi(portStr); // Return the port number
 }
--- ipv4.cpp
+++ ipv4.cpp
@@ -8,14 +8,14 @@
 #include <iostream> //declares objects that control reading from and writing to the standard streams
 #include <cstring> //tracks the string length for faster performance, but it also retains the NULL character in the stored character data to support conversion to LPCWSTR
 // Define the structure of a UDP header
-struct udp_header {
-    uint16_t src_port;
+struct udp_header { // Define the structure of a UDP header
+    uint16_t src_port; 
     uint16_t dest_port;
     uint16_t len;
     uint16_t checksum;
 };
 
-struct pseudo_header {
+struct pseudo_header { // Define the structure of a pseudo header
     struct in_addr src;
     struct in_addr dest;
     uint8_t zero; // placeholder for the zero byte
@@ -23,7 +23,7 @@ struct pseudo_header {
     uint16_t udp_length;
 };
 
-uint16_t computeChecksum(const char *buffer, int len) {
+uint16_t computeChecksum(const char *buffer, int len) { // computes the checksum
     uint32_t computedChecksum = 0;
     const uint16_t *word;
 
@@ -46,36 +46,36 @@ uint16_t computeChecksum(const char *buffer, int len) {
 
 std::string getSecretPhrase(const char* ip, int port, uint32_t signature) {
     struct sockaddr_in serverAddr;
-    int udpsock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    int udpsock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP); // Create a UDP socket
     if (udpsock < 0) {
         perror("Error creating UDP socket ");
     }
-    if (udpsock < 0) {
+    if (udpsock < 0) { 
         return "error"; // If error creating an udp socket
     }
     // Configure settings in address struct
-    memset(&serverAddr, 0, sizeof(serverAddr));
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(port);
-    inet_pton(AF_INET, ip, &serverAddr.sin_addr);
-    struct timeval timeout;
-    timeout.tv_sec = 5;
+    memset(&serverAddr, 0, sizeof(serverAddr)); // Zero out memory of address struct
+    serverAddr.sin_family = AF_INET; // IPv4
+    serverAddr.sin_port = htons(port); // Assigns the port to the sin struct
+    inet_pton(AF_INET, ip, &serverAddr.sin_addr); // Assigns the ip to the sin struct
+    struct timeval timeout; // Set timeout for socket to wait for a reply
+    timeout.tv_sec = 5; // 5 second timeout
     timeout.tv_usec = 0;
     // Set timeout for socket to wait for a reply
-    if (setsockopt(udpsock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0) {
+    if (setsockopt(udpsock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0) { // sets timeout for socket
         perror("Error setting options");
         close(udpsock);
         return "error";
     }
 
-    if (sendto(udpsock, &signature, sizeof(signature), 0, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+    if (sendto(udpsock, &signature, sizeof(signature), 0, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) { // Send the signature
         perror("Error sending data");
         close(udpsock);
         return "error";
     } 
-    char recvBuffer[1024];
-    socklen_t addr_size = sizeof(serverAddr);
-    int recvBytes = recvfrom(udpsock, recvBuffer, 1024, 0, (struct sockaddr*)&serverAddr, &addr_size);
+    char recvBuffer[1024]; // Buffer to receive data into
+    socklen_t addr_size = sizeof(serverAddr); // Size of address struct
+    int recvBytes = recvfrom(udpsock, recvBuffer, 1024, 0, (struct sockaddr*)&serverAddr, &addr_size); // Receive data
 
     if (recvBytes < 0) {
         perror("Error receiving data");
@@ -90,7 +90,7 @@ std::string getSecretPhrase(const char* ip, int port, uint32_t signature) {
         
         // Extracting the checksum from the first 2 bytes and converting it from network byte order
         
-        uint16_t requiredChecksum = *((uint16_t *)lastSixBytes);
+        uint16_t requiredChecksum = *((uint16_t *)lastSixBytes); // Extracting the checksum from the first 2 bytes
         
         struct in_addr addr;
         memcpy(&addr, &lastSixBytes[2], 4); // Copy the 4 bytes for the IPv4 address
@@ -100,11 +100,11 @@ std::string getSecretPhrase(const char* ip, int port, uint32_t signature) {
         std::cout << "Extracted source address: " << requiredIP << std::endl;
 
         // Create UDP pseudo-header for checksum calculation
-        struct pseudo_header ph;
-        ph.src.s_addr = inet_addr(requiredIP);
-        ph.dest.s_addr = serverAddr.sin_addr.s_addr;
-        ph.zero = 0;
-        ph.protocol = IPPROTO_UDP;
+        struct pseudo_header ph; // Create UDP pseudo-header for checksum calculation
+        ph.src.s_addr = inet_addr(requiredIP); // Source address
+        ph.dest.s_addr = serverAddr.sin_addr.s_addr; // Destination address
+        ph.zero = 0; 
+        ph.protocol = IPPROTO_UDP; // UDP protocol
         ph.udp_length = htons(sizeof(udp_header) + sizeof(signature));  // Signature is payload
 
         // Create UDP header
@@ -118,20 +118,20 @@ std::string getSecretPhrase(const char* ip, int port, uint32_t signature) {
         char buf[sizeof(pseudo_header) + sizeof(udp_header) + sizeof(signature)];
         
         udp.checksum = 0;  // Set to 0 initially for calculation
-        memcpy(buf, &ph, sizeof(pseudo_header));
-        memcpy(buf + sizeof(pseudo_header), &udp, sizeof(udp_header));
-        memcpy(buf + sizeof(pseudo_header) + sizeof(udp_header), &signature, sizeof(signature));
+        memcpy(buf, &ph, sizeof(pseudo_header));  // Copy pseudo header into buffer
+        memcpy(buf + sizeof(pseudo_header), &udp, sizeof(udp_header)); // Copy UDP header into buffer
+        memcpy(buf + sizeof(pseudo_header) + sizeof(udp_header), &signature, sizeof(signature)); // Copy signature into buffer
 
         // Calculate UDP checksum including the pseudo header
-        uint16_t calculated_checksum = computeChecksum(buf, sizeof(buf));
+        uint16_t calculated_checksum = computeChecksum(buf, sizeof(buf));  // Calculate UDP checksum including the pseudo header
 
         // If the checksum isn't correct, change the source port or data slightly and recompute until it matches.
         int attempts = 0;
-        while (calculated_checksum != requiredChecksum ) { //&& attempts < 65535
+        while (calculated_checksum != requiredChecksum ) { // If the checksum isn't correct, change the source port or data slightly and recompute until it matches.
             udp.src_port = htons(rand() % 65535);
             // repeat the buffer creation and checksum calculation process untill it matches the required one
-            memcpy(buf + sizeof(pseudo_header), &udp, sizeof(udp_header));
-            calculated_checksum = computeChecksum(buf, sizeof(buf));
+            memcpy(buf + sizeof(pseudo_header), &udp, sizeof(udp_header)); // Copy UDP header into buffer
+            calculated_checksum = computeChecksum(buf, sizeof(buf)); // Calculate UDP checksum including the pseudo header
             
             if(attempts == 65535) {
                 attempts = 0;
@@ -139,11 +139,11 @@ std::string getSecretPhrase(const char* ip, int port, uint32_t signature) {
             attempts++;
         }
         std::cout << "attempts: " << attempts << std::endl;
-        if (attempts == 65535) {
+        if (attempts == 65535) { // returns error if the checksum has not been found
             std::cerr << "Failed to generate packet with desired checksum after 65,535 attempts. Please, try again." << std::endl;
             return "error"; 
         }
-        udp.checksum = calculated_checksum; //
+        udp.checksum = calculated_checksum; // Assign the calculated checksum to the udp checksum
         //uint8_t first = flipChecksum 
 /////////// Now send the UDP packet
         char packet[sizeof(struct ip) + sizeof(udp_header) + sizeof(signature)]; // Signature as the payload
@@ -153,32 +153,32 @@ std::string getSecretPhrase(const char* ip, int port, uint32_t signature) {
         char *payload = packet + sizeof(struct ip) + sizeof(udp_header);
 
         // Fill the IP header fields
-        iph->ip_v = 4;
-        iph->ip_hl = 5;
-        iph->ip_tos = 0;
-        iph->ip_len = htons(sizeof(packet));
-        iph->ip_id = 0;
-        iph->ip_off = 0;
-        iph->ip_ttl = 64;
-        iph->ip_p = IPPROTO_UDP;
+        iph->ip_v = 4; // IPv4
+        iph->ip_hl = 5; // Header length
+        iph->ip_tos = 0; // Type of service
+        iph->ip_len = htons(sizeof(packet)); // Total length
+        iph->ip_id = 0; // Identification
+        iph->ip_off = 0;    // Fragment offset field
+        iph->ip_ttl = 64;  // Time to live
+        iph->ip_p = IPPROTO_UDP; // UDP
         iph->ip_sum = 0;  // Set to zero before calculating
         iph->ip_sum = computeChecksum((const char *)iph, sizeof(struct ip)); // ip checksum
         iph->ip_src.s_addr = inet_addr(requiredIP);
         iph->ip_dst.s_addr = serverAddr.sin_addr.s_addr;
 
         // Copy udp_header and payload into packet
-        memcpy(udph, &udp, sizeof(udp));
-        memcpy(payload, &signature, sizeof(signature));
+        memcpy(udph, &udp, sizeof(udp)); // Copy udp_header into packet
+        memcpy(payload, &signature, sizeof(signature)); // Copy payload into packet
 
         // Now send the packet
-        if (sendto(udpsock, packet, sizeof(packet), 0, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) {
+        if (sendto(udpsock, packet, sizeof(packet), 0, (struct sockaddr*)&serverAddr, sizeof(serverAddr)) < 0) { // Send the packet
             perror("Error sending UDP packet.");
             close(udpsock);
             return "error";
         }
         char recvBufferTwo[1024];
         socklen_t addr_size = sizeof(serverAddr);
-        int recvBytesTwo = recvfrom(udpsock, recvBufferTwo, 1024, 0, (struct sockaddr*)&serverAddr, &addr_size);
+        int recvBytesTwo = recvfrom(udpsock, recvBufferTwo, 1024, 0, (struct sockaddr*)&serverAddr, &addr_size); // Receive data
         
         if (recvBytesTwo < 0) {
             perror("Error receiving second data.");
@@ -188,7 +188,7 @@ std::string getSecretPhrase(const char* ip, int port, uint32_t signature) {
 
         // Now extract the secret phrase
         char secretPhrase[512];
-        if (sscanf(recvBufferTwo, "Congratulations group 99! Here is the secret phrase: \"%[^\"]", secretPhrase) == 1) {
+        if (sscanf(recvBufferTwo, "Congratulations group 99! Here is the secret phrase: \"%[^\"]", secretPhrase) == 1) { // Now extract the secret phrase
             std::cout << "\nSecret Phrase: " << secretPhrase << std::endl;
             return std::string(secretPhrase);
         }
